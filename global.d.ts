e6f8fe501f3cf8866f6fbc25da4a32605c17ab98
apo5698 <winteryao17@gmail.com>
2024-09-25 00:39:12 -0400

    chore: remove global interfaces

--- global.d.ts
+++ global.d.ts
@@ -1,14 +1,1 @@
-export declare global {
-  interface Bath {
-    id: number;
-    petId: number;
-    time: string;
-  }
-
-  interface Weight {
-    id: number;
-    petId: number;
-    weight: number;
-    time: string;
-  }
-}
+export declare global {}
